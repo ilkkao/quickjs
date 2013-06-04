d9cc4653f3af996a611eefc920e7ff42f43080f2
Ilkka Oksanen <iao@iki.fi>
2013-06-04 17:48:59 +0200

    Added source mode to build.sh
    ./build.sh --source generates quick.js file which loads other
    javascript files at runtime. In this mode it's possible to develop
    quickjs without the need to run ./build.sh constantly.
    
    Tested with Mac using file:// urls.

---
+++ src/loader.js
@@ -0,0 +1,40 @@
+
+//Simple JavaScript loader.
+//
+//Note that there is race between the loader and <body onload="Quick.run();">
+//Loader should call Quick.run(). Then built version could have load.js
+//which just does body onload=Quick.run() in javascript.
+//
+//At least with file:// urls, race is not an issue.
+//
+
+(function() {
+    var files = ['tokenizer', 'compiler', 'dom', 'animation', 'helper',
+                 'engine'];
+    var head = document.getElementsByTagName('head')[0];
+    var scripts = document.getElementsByTagName('script');
+    var loaded = 0;
+    var baseUrl = "";
+
+    //First figure out from where quick.js was loaded
+    for (var i = 0; i < scripts.length; i++) {
+        var url = scripts[i].src;
+        if (url && url.match(/quick.js$/)) {
+            baseUrl = url.replace('quick.js','');
+            break;
+        }
+    }
+
+    for (var i = 0; i < files.length; i++) {
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.src = baseUrl + 'src/' + files[i] + '.js';
+        script.onload = function () {
+            if (++loaded == files.length) {
+                console.log('All JavaScript files loaded');
+            }
+        };
+
+        head.appendChild(script);
+    }
+}());
