637b0184f03c9098657ee10de23d62ae939a7656
Ilkka Oksanen <iao@iki.fi>
2013-06-04 17:48:59 +0200

    Added source mode to build.sh
    ./build.sh --source generates quick.js file which loads other
    javascript files at runtime. In this mode it's possible to develop
    quickjs without the need to run ./build.sh constantly.
    
    Tested with Mac using file:// urls.

--- build.sh
+++ build.sh
@@ -8,32 +8,47 @@ MINIFIER_OPTIONS=""
 MODULES_FOLDER="./modules/"
 QUICKJS=$PWD/bin/quickjs
 QUICKJS_OPTIONS="-s"
+SOURCE_MODE=false
+
+echo ""
+
+if [ "$1" = "--source" ]; then
+    SOURCE_MODE=true
+fi
 
 if command -v uglifyjs >/dev/null 2>&1; then
     MINIFIER=uglifyjs
 else
-    echo "WARN: UglifyJS not found. Skipping minification steps."
+    echo "WARN: UglifyJS not found. Skipping minification."
 fi
 
-#### batching all quickjs files into one
-echo "Create batched $OUT file."
-echo " -> Removing old combined $OUT file..."
+echo " -> Removing old $OUT file..."
 rm -f $OUT $OUT_MINIFIED
 
-for file in $INPUT; do
-    echo " -> Adding $file ..."
-    cat src/$file.js >> $OUT
-done
-
-echo "Done."
-echo ""
-
-#### minify combined file
-if [ -n "$MINIFIER" ]; then
-    echo "Minify $OUT to $OUT_MINIFIED."
-    $MINIFIER $MINIFIER_OPTIONS $OUT -o $OUT_MINIFIED
+#### batching all quickjs files into one
+if $SOURCE_MODE ; then
+    echo "Create $OUT as a dynamic loader file."
+    cp src/loader.js $OUT
     echo "Done."
     echo ""
+else
+    echo "Create batched $OUT file."
+
+    for file in $INPUT; do
+        echo " -> Adding $file ..."
+        cat src/$file.js >> $OUT
+    done
+
+    echo "Done."
+    echo ""
+
+    #### minify combined file
+    if [ -n "$MINIFIER" ]; then
+        echo "Minify $OUT to $OUT_MINIFIED."
+        $MINIFIER $MINIFIER_OPTIONS $OUT -o $OUT_MINIFIED
+        echo "Done."
+        echo ""
+    fi
 fi
 
 #### Generating modules
@@ -42,10 +57,11 @@ for file in `find $MODULES_FOLDER -name "*.jml"`; do
     echo " -> Process $file..."
     echo "  -> Generate JavaScript from JML..."
     $QUICKJS $QUICKJS_OPTIONS $file $file.js
-if [ -n "$MINIFIER" ]; then
-    echo "  -> Minify $file.js to $file.min.js"
-    $MINIFIER $MINIFIER_OPTIONS $file.js -o $file.min.js
-fi
+    if [ -n "$MINIFIER" ]; then
+        echo "  -> Minify $file.js to $file.min.js"
+        $MINIFIER $MINIFIER_OPTIONS $file.js -o $file.min.js
+    fi
 done
+
 echo "Done."
 echo ""
